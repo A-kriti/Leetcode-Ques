af0f388c73a62443e9133dfff2558360eae0d6ff
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-10-07 01:17:02 +0530

    Create Number of 1 Bits.cpp

---
+++ Practice/Number of 1 Bits.cpp
@@ -0,0 +1,20 @@
+// ques - https://leetcode.com/problems/number-of-1-bits/submissions/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        
+        int c=0;
+        
+        while(n!=0){
+            
+            if(n&1){
+                c++;
+            }
+            
+            n=n>>1;
+        }
+        
+        return c;
+    }
+};
