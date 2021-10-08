50f28c8453e1577b25129a52a820eedbf44131e6
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-10-08 10:28:03 +0530

    Create Reverse Linked List.cpp

---
+++ Data Structure I/Reverse Linked List.cpp
@@ -0,0 +1,23 @@
+// ques - https://leetcode.com/problems/reverse-linked-list/submissions/
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* next=NULL;
+                
+        while(curr!=NULL){
+            
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            
+        }
+                    
+        return prev;      
+       
+    }
+};
