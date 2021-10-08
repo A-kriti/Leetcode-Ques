bcd3c4abc2bc988b9bdf7e30f7ee6a033102d4e8
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-10-08 14:11:59 +0530

    Update Linked List Cycle.cpp

--- Data Structure I/Linked List Cycle.cpp
+++ Data Structure I/Linked List Cycle.cpp
@@ -14,6 +14,30 @@ public:
     
     bool hasCycle(ListNode *head) {
         
+        //Method 1: 2 Pointers
+            
+        ListNode *slow=head;
+        ListNode *fast=head;
+        
+        
+        while(fast!=NULL && fast->next!=NULL){
+            
+            slow=slow->next;
+            fast=fast->next->next;
+            
+            if(slow==fast){
+                return true;
+            }
+            
+        }
+        
+        
+        return false;
+        
+        
+        /*
+        Method 2: Hashmap
+        
         unordered_map<ListNode *,int>m;
         
         while(head!=NULL){
@@ -31,6 +55,6 @@ public:
             
         }
         
-       return false; 
+       return false; */
     }
 };
