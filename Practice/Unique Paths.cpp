06b64dcc616ae4d5b45d2a1e0a1771e8d7e1f4ae
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-10-19 00:35:58 +0530

    Create Unique Paths.cpp

---
+++ Practice/Unique Paths.cpp
@@ -0,0 +1,35 @@
+// ques - https://leetcode.com/problems/unique-paths/submissions/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        int t[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                
+                if(i==0  || j==0){
+                    t[i][j]=1;
+                }
+                else{
+                    t[i][j]=0;
+                }   
+            
+            }
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                
+                
+                    t[i][j]=t[i-1][j]+t[i][j-1];
+                
+            }
+        }
+        
+        
+        return t[m-1][n-1];
+        
+    }
+};
