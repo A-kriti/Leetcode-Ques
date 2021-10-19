4eeed0fd315d0113b532a41c7dd90b1536668e99
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-10-20 00:58:56 +0530

    Create Hamming Distance.cpp

---
+++ Practice/Hamming Distance.cpp
@@ -0,0 +1,22 @@
+// ques - https://leetcode.com/problems/hamming-distance/
+
+class Solution {
+public:
+    
+    
+    int hammingDistance(int x, int y) {
+      
+        
+        int c=0;
+        for(int i=0;i<32;i++){
+            
+            if((x>>i&1) != (y>>i&1))
+                c++;
+            
+        } 
+        
+        return c;
+        
+        
+    }
+};
