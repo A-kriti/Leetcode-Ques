09003afdd4689a832ae0a12f3f0a11a7a1d44c47
Akriti <73133055+A-kriti@users.noreply.github.com>
2021-11-09 16:51:04 +0530

    Create Concatenation of Array.cpp

---
+++ Practice/Concatenation of Array.cpp
@@ -0,0 +1,15 @@
+// ques - https://leetcode.com/problems/concatenation-of-array/submissions/
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            nums.push_back(nums[i]);
+        }
+        
+        return nums;
+        
+    }
+};
