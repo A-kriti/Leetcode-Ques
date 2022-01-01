4e89c5d42bc46b437e7defcc731bc1a932a3a960
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-01-01 18:51:12 +0530

    Update Intersection of Two Arrays.cpp

--- Practice/Intersection of Two Arrays.cpp
+++ Practice/Intersection of Two Arrays.cpp
@@ -4,30 +4,55 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         
+
+        // method 1
+        map<int,int>m;
+        vector<int>v;
         
-        vector<int> v;
-        unordered_set<int> t;
+        for(int x:nums1){
+            m[x]++;
+        }
         
-        int x=nums1.size();
-        int y=nums2.size();
-        
-        for(int i=0;i<x;i++){
-            for(int j=0;j<y;j++){
+        for(int x:nums2){
             
-                 if(nums2[j]==nums1[i]){
+            if(m.find(x)!=m.end()){
+                m[x]=1005;
                 
-                if(t.find(nums1[i])==t.end())   
-                    t.insert(nums1[i]);
-                }
-                     
             }
         }
         
-        for ( auto it = t.begin(); it != t.end(); ++it ){
-            v.push_back(*it);
+        for(auto x:m){
+            
+            if(x.second==1005)v.push_back(x.first);
+
         }
         
-       return v; 
+        return v;
+
+         // method 2
+//         vector<int> v;
+//         unordered_set<int> t;
+        
+//         int x=nums1.size();
+//         int y=nums2.size();
+        
+//         for(int i=0;i<x;i++){
+//             for(int j=0;j<y;j++){
+            
+//                  if(nums2[j]==nums1[i]){
+                
+//                 if(t.find(nums1[i])==t.end())   
+//                     t.insert(nums1[i]);
+//                 }
+                     
+//             }
+//         }
+        
+//         for ( auto it = t.begin(); it != t.end(); ++it ){
+//             v.push_back(*it);
+//         }
+        
+//        return v; 
         
     }
 };
