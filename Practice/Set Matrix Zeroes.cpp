95a5bfb9d479a820429094656157f1ddad4e32d2
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-01-02 15:43:59 +0530

    Update Set Matrix Zeroes.cpp

--- Practice/Set Matrix Zeroes.cpp
+++ Practice/Set Matrix Zeroes.cpp
@@ -1,5 +1,7 @@
 // ques - https://leetcode.com/problems/set-matrix-zeroes/
 
+
+//method 1
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -41,6 +43,45 @@ public:
             }
         }
             
+        
+    }
+};
+
+//method 2
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        int row=matrix.size();
+        int col=matrix[0].size();
+        
+        vector<int>v_row(row,1);
+        vector<int>v_col(col,1);
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                
+                if(matrix[i][j]==0){
+                    v_row[i]=0;
+                    v_col[j]=0;
+                }
+            
+            }
+        }
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                
+                if(v_row[i]==0 || v_col[j]==0){
+                    matrix[i][j]=0;
+                    
+                }
+            
+            }
+        }
+        
+       
         
     }
 };
