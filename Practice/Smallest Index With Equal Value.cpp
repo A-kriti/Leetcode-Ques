1fb85b124a80775e36685917de27cc31b9359432
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-01-04 00:41:03 +0530

    Create Smallest Index With Equal Value.cpp

---
+++ Practice/Smallest Index With Equal Value.cpp
@@ -0,0 +1,14 @@
+// ques - https://leetcode.com/problems/smallest-index-with-equal-value/
+
+class Solution {
+public:
+    int smallestEqual(vector<int>& nums) {
+        
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            
+            if(i%10==nums[i])return i;
+        }
+        return -1;
+    }
+};
