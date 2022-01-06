5c08956b28e58ae4ee72ef2cad9255577836a1bb
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-01-06 22:44:09 +0530

    Create Flipping an Image.cpp

---
+++ Practice/Flipping an Image.cpp
@@ -0,0 +1,27 @@
+// ques - https://leetcode.com/problems/flipping-an-image/
+
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        
+        int n=image.size();
+        
+        for(int i=0;i<n;i++){
+            
+            reverse(image[i].begin(),image[i].end());
+        }
+        
+        for(int i=0;i<n;i++){
+            
+            for(int j=0;j<n;j++){
+            
+                if(image[i][j]==1)image[i][j]=0;
+                
+                else if(image[i][j]==0)image[i][j]=1;
+            }
+        }
+        
+        return image;
+        
+    }
+};
