894d64e3cffbf842ac92443faa6a956dee4bb808
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-01-27 17:03:01 +0530

    Create Contains Duplicate II.cpp

---
+++ Practice/Contains Duplicate II.cpp
@@ -0,0 +1,19 @@
+// ques - https://leetcode.com/problems/contains-duplicate-ii/
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            if(m.find(nums[i])!=m.end()){
+                if(i-m[nums[i]]<=k)return true;
+            }
+             m[nums[i]]=i;
+        }
+        
+        return false;
+        
+        
+    }
+};
