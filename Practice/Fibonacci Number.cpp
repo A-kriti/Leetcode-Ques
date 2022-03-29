6ac51c9a0cd89fadfc52407a28f66e80c38572f8
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-03-29 12:00:25 +0530

    Create Fibonacci Number.cpp

---
+++ Practice/Fibonacci Number.cpp
@@ -0,0 +1,26 @@
+// ques - https://leetcode.com/problems/fibonacci-number/
+
+class Solution {
+public:
+    int fib(int n) {
+        
+        if(n==0){
+            return 0;
+        }
+        
+        if(n==1){
+            return 1;
+        }
+        
+        int t[n+1];
+        
+        t[0]=0;
+        t[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            t[i]=t[i-1]+t[i-2];
+        }
+        
+        return t[n];
+    }
+};
